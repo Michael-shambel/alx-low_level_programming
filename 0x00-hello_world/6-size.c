c1bacfd95459633380bc7a6885b3f5b036c3e5e4
Michael-shambel <104815406+Michael-shambel@users.noreply.github.com>
2022-06-05 19:54:50 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-<<<<<<< HEAD
 	printf("size of a char: %i byte(s)\n", sizeof(char));
 	printf("size of an int: %i byte(s)\n", sizeof(int));
 	printf("size of a long int: %i byte(s)\n", sizeof(long int));
