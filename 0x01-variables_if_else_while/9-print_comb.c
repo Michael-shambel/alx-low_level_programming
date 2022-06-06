a76bc1820c7911becb48e4e21ca365d699fc923f
Michael Shambel <Michaelshambel10@gmail.com>
2022-06-06 13:29:52 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print comb
+ *
+ * Return: Always(success)
+ */
+int main(void)
+{
+int a;
+for (a = '0';  <= '9'; a++)
+{
+putchar(a);
+if (a != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
