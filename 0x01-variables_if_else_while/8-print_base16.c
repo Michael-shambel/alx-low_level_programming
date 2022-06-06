35102a5263f3395910baed08a0deae1ac7a24840
Michael Shambel <Michaelshambel10@gmail.com>
2022-06-06 13:15:34 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include<stdoi.h>
+
+/**
+ * main - print heximal
+ *
+ * Return: Always(success)
+ */
+int main(void)
+{
+char w;
+for (w = '0'; w <= '9'; w++)
+{
+putchar(w);
+}
+for (w = 'a'; w <= '9'; w++)
+{
+putchar(w);
+}
+putchar('\n');
+return (0);
+}
