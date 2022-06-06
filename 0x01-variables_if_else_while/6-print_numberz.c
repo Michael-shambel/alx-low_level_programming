70870cd7346a1d4bec0ffb7df3ee964d790d9e8c
Michael Shambel <Michaelshambel10@gmail.com>
2022-06-06 12:59:55 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - print numerz
+ *
+ * Return: Always (success)
+ */
+int main(void)
+{
+int w;
+for (w = '0'; w <= '9'; w++)
+{
+putchar(w);
+}
+putchar('\n');
+return (0);
+}
