09cc6e05c05ed7cfd377c2d0fe0ae65c752ab7fb
Michael Shambel <Michaelshambel10@gmail.com>
2022-06-06 12:50:34 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - print number base
+ *
+ * Return: Always(success)
+ */
+int main(void)
+{
+char w;
+for (w = '0'; w <= '9'; w++)
+{
+putchar(w);
+}
+putchar('\n');
+return (0);
+}
