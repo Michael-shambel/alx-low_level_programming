678b12fd582684b62f18848568eeecb8f3084d6d
Michael Shambel <Michaelshambel10@gmail.com>
2022-06-10 02:59:22 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,17 +4,19 @@
  */
 void print_alphabet_x10(void)
 {
-	char alpha;
-	int i;
-	i = 0;
-	while (i < 10)
-	{
-		alpha = 'a';
-		while (alpha <= 'z')
-		{
-			_putchar(alpha);
-			alpha++;
-		}
-		_putchar('\n');
-	}
+char alpha;
+int i;
+
+i = 0;
+
+while (i < 10)
+{
+alpha = 'a';
+while (alpha <= 'z')
+{
+_putchar(alpha);
+alpha++;
+}
+_putchar('\n');
+}
 }
