1712371e15c39ee44f689d94ef9ee4b8256bb409
Michael Shambel <Michaelshambel10@gmail.com>
2022-06-10 12:22:22 -0700

    task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,9 @@
 #include "main.h"
+
 /**
  * add - add two integer and display the result
- * @a: integer to be add to b
- * @b: integer to be add to a
+ * @a: int to be add to b
+ * @b: int to be add to a
  * Return: sum of a and b
  */
 void add(int a, int b)
