183fa1d7a88b2c3ed4baaf1566bc1512fbd2e656
Michael Shambel <Michaelshambel10@gmail.com>
2022-06-12 12:06:29 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers
+ */
+void more_numbrs(void)
+{
+int i, j;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 15; j++)
+{
+_putchar(y + '0');
+}
+_putchar('\n');
+}
+}
