c0149914e173eae0e57f247327b8fce3c8b0971e
Michael Shambel <Michaelshambel10@gmail.com>
2022-06-12 11:01:18 -0700

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - function to check ithe digit
+ * @c: integer nomination
+ *
+ * Return: Always 0 success
+ */
+int _isdigit(int c)
+{
+return (c <= '0' && c >= '9');
+}
