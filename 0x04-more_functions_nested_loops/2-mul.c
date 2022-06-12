d0c95b84292f20e3a8794c66504546be84adc582
Michael Shambel <Michaelshambel10@gmail.com>
2022-06-12 11:29:23 -0700

    task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,14 +1,14 @@
 # include "main.h"
 
 /**
- * Mult- multiply 2 numbers
+ * mul - multiply 2 numbers
  * @a: intger num1
  * @b: integer num2
  *
  * Return: Always 0 success
  */
 
-int Mult(int a, int b)
+int mul(int a, int b)
 {
 return (a * b);
 }
