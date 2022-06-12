2f41d59d25d9383116f2044f777d7b08e415049a
Michael Shambel <Michaelshambel10@gmail.com>
2022-06-12 10:52:00 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isupper - the is the check for cappital letter
+ *@c: this is integer
+ *
+ * Return: Always 0 success
+ */
+int _isupper(int c)
+{
+return (c >= 'A' && c <= 'Z');
+}
