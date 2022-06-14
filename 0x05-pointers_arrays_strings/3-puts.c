f870b4b32fc67cf04fdeea6c0fce495b2922b5dd
Michael Shambel <Michaelshambel10@gmail.com>
2022-06-14 13:03:07 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - this is the function of puts
+ * @str: a param to puts function
+ */
+void _puts(char *str)
+{
+for (; *str != '\0'; str++)
+{
+_putchar(*str);
+}
+_putchar('\n');
+}
