176d047d4e66b14f951bc03578786a40b8835ca0
Michael Shambel <Michaelshambel10@gmail.com>
2022-06-14 13:56:19 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,19 +2,18 @@
 #include <string.h>
 
 /**
- * puts_half(char *str)
+ * puts_half  - is thefunction
  * @str: string param
  */
 void puts_half(char *str)
 {
-	int x, y, i;
-	x = strlen(str);
-	if (x % 2 == 1)
-		y = x / 2 + 1;
-	else
-		y = x / 2;
-	for (i = y; i < x; i++)
-		_putchar(str[i]);
-
-	_putchar('\n');
+int x, y, i;
+x = strlen(str);
+if (x % 2 == 1)
+y = x / 2 + 1;
+else
+y = x / 2;
+for (i = y; i < x; i++)
+_putchar(str[i]);
+_putchar('\n');
 }
