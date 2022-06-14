6ccf018fc61c7e696d44ea532515316cbd3435d6
Michael Shambel <Michaelshambel10@gmail.com>
2022-06-14 11:26:43 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * rest_to_98 - function to update the pointe
+ * reset_to_98 - function to update the pointe
  * @n: pointer to n to update
  */
-void rest_to_98(int*n)
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
