290fff8798ca488f20a5cc705b7bbe420daf9f34
Michael Shambel <Michaelshambel10@gmail.com>
2023-09-08 11:24:15 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,15 +10,10 @@ int main(void)
 	char CAP;
 
 	for (CAP = 'a'; CAP <= 'Z'; CAP++)
-	{
 	putchar(CAP);
-	}
-
 
 	for (CAP = 'A'; CAP <= 'Z'; CAP++)
-	{
 	putchar(CAP);
-	}
 	putchar('\n');
 	return (0);
 }
