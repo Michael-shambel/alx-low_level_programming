06737f98c1b48329c51206d6251603d34289f81b
Michael Shambel <Michaelshambel10@gmail.com>
2023-09-09 14:45:23 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,16 +13,16 @@ int k;
 int l;
 for (i = 0; i < 10; i++)
 {
-for (j = 0; j <10; j++)
+for (j = 0; j < 10; j++)
 {
 putchar(i + '0');
 putchar(j + '0');
 putchar(' ');
 for (k = 0; k < 10; k++)
 {
-for (l= k + 1; k < 10; k++)
+for (l = k + 1; l < 10; l++)
 {
-putchar(k +'0');
+putchar(k + '0');
 putchar(l + '0');
 if ((i != 9) || (j != 8) || (k != 9) || (l != 9))
 {
