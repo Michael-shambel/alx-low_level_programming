530b4855e378fa29fec0b17af2d4f6b329ee78e7
Michael Shambel <Michaelshambel10@gmail.com>
2023-09-13 15:18:13 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_sign - print he sign of a number
+ *@n: add value n
+ * Return: 1 if it is posetive or 0 anthing else
+ */
+int print_sign(int n)
+{
+	if (n >= 48 && n <= 57)
+	{
+		return (1);
+	}
+	return (0);
+}
+
