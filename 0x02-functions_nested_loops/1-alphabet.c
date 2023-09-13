b4e3ef0cc48f7a4de199b4ee0798fc3be871f5b2
Michael Shambel <Michaelshambel10@gmail.com>
2023-09-13 13:50:10 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -16,6 +16,7 @@ for (i = 'a'; i <= 'z'; i++)
 _putchar(i);
 }
 _putchar('\n');
+return;
 
 }
 
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+int i = 0;
+char c;
+while(i < 10)
+{
+for(c = 'a'; c <= 'z';c++)
+{
+_putchar(c);
+}
+i++;
+_putchar('\n');
+}
+return ;
+}
