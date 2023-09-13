4b6044deea4cec7e54795f4e5b0502a445ce9a29
Michael Shambel <Michaelshambel10@gmail.com>
2023-09-13 13:59:08 -0700

    2-print_alphabet_x10.c:24: WARNING: space prohibited before semicolon

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,14 +10,16 @@ void print_alphabet_x10(void)
 {
 int i = 0;
 char c;
-while(i < 10)
+while (i < 10)
 {
-for(c = 'a'; c <= 'z';c++)
+for (c = 'a'; c <= 'z'; c++)
 {
 _putchar(c);
 }
 i++;
+
 _putchar('\n');
+
 }
 return ;
 }
