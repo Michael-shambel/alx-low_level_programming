b1ba1770b4f14b7ae043f9d93da4f7f006e96a53
Michael Shambel <Michaelshambel10@gmail.com>
2023-09-13 09:30:37 -0700

    ./1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+	putchar(i);
+}
+putchar('\n');
+
+}
+
