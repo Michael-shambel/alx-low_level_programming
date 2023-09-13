f4ebe6de85f4939de60f5e14aed5ca54e4ef7f6c
Michael Shambel <Michaelshambel10@gmail.com>
2023-09-13 15:06:17 -0700

    ./4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha -Chack for characer are alphabetic
+ *@c: the characer
+ * Return: 1 if c is letter or 0 anything else
+ */
+int _isalpha(int c)
+{
+	if((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return 1;
+	}
+	return 0;
+}
+
