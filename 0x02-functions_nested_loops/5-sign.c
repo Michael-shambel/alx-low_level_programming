380d4edf7b985fc0fdd882ee3d7889113333907b
Michael Shambel <Michaelshambel10@gmail.com>
2023-09-13 15:47:13 -0700

    ./5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,10 +7,20 @@
  */
 int print_sign(int n)
 {
-	if (n >= 48 && n <= 57)
+	if (n > 0)
 	{
+		_putchar(43);
 		return (1);
 	}
-	return (0);
+	else if ( n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
 }
 
