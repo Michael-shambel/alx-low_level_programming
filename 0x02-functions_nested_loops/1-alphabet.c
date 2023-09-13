ed45ae2c8cc0162e4d3ea5c8479f5e52d787fe3c
Michael Shambel <Michaelshambel10@gmail.com>
2023-09-13 14:36:58 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,9 +3,9 @@
 
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * _islower - checks for lowercase character
+ *@c: The character to be checked
+ * Return: 1 for lowercase character for 0 for anything else
  */
 
 void print_alphabet(void)
