68695c17e7f7a814b9c819d09b74b5228be80b39
Michael Shambel <Michaelshambel10@gmail.com>
2023-09-13 18:00:55 -0700

    ./10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two values
+ * @num1 - first number
+ * @num2 - second number
+ * Return: total value
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
+
