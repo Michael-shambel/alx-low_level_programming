0ae79196b5441723a9260b18cbb70904b3eb2562
Michael Shambel <Michaelshambel10@gmail.com>
2023-09-15 16:11:00 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - print it as square
+ *@size - number of hushtag
+ * Return: Always 0.
+ */
+void print_square(int size)
+{
+	int x, y;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
