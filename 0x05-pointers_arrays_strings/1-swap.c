18ea58c892e422ad0fb97a9402ab0089e823844d
Michael Shambel <Michaelshambel10@gmail.com>
2023-09-18 11:02:15 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *swap_int - swap to integers
+ *@a: firs intiger
+ *@b: second intiger
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
