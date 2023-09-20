b29da08273ee234fa7948b66f335bda1c93b8130
Michael Shambel <Michaelshambel10@gmail.com>
2023-09-20 11:37:18 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,25 +15,25 @@ for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 10; j++)
 {
-putchar(i + '0');
-putchar(j + '0');
-putchar(' ');
-}
-}
 for (k = 0; k < 10; k++)
 {
 for (l = k + 1; l < 10; l++)
 {
+putchar(i + '0');
+putchar(j + '0');
+putchar(' ');
 putchar(k + '0');
 putchar(l + '0');
-}
-}
 if ((i != 9) || (j != 8) || (k != 9) || (l != 9))
 {
 putchar(',');
 putchar(' ');
-}
 
+}
+}
+}
+}
+}
 putchar('\n');
 return (0);
 
