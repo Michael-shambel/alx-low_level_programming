3e5836973634b6b38a6b620574d639728d6acd19
Michael Shambel <Michaelshambel10@gmail.com>
2023-10-04 00:48:08 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - new memory space allocator
+ *@str: allocate char
+ * Return: Alx
+ */
+char *_strdup(char *str)
+{
+	char *alx;
+	int i = 0, j = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i])
+		i++;
+	alx = malloc((sizeof(char) * i) + 1);
+	while (str[j])
+	{
+		alx[j] = str[j];
+		j++;
+	}
+	return (alx);
+}
