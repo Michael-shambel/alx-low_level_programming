766ef86a325e39492783628e4a22641c43dcdb0f
Michael Shambel <Michaelshambel10@gmail.com>
2023-10-07 03:44:03 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,17 +8,17 @@
  */
 int main(void)
 {
-	long i, max;
-	long num = 612852475143;
-	double num1 = sqrt(num);
+long i, max;
+long num = 612852475143;
+double num1 = sqrt(num);
 
-	while (i = 1; i <= num1; i++)
-	{
-		if (num % i == 0)
-		{
-			max = num / i;
-		}
-	}
-	printf("%ld\n", max);
-	return (0);
+for (i = 1; i <= num1; i++)
+{
+if (num % i == 0)
+{
+max = num / i;
+}
+}
+printf("%ld\n", max);
+return (0);
 }
