9997071ad8e2c1b28fc5210a6631caf4182b3780
Michael Shambel <Michaelshambel10@gmail.com>
2023-10-10 13:45:54 -0700

    dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,6 +13,13 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+ * dog_t - change the name
+ *
+ */
+typedef struct dog dog_t;
+
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
