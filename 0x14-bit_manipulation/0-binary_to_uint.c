fb4518bf8551976b6ee17499ca31e1b21ad6934c
Michael Shambel <Michaelshambel10@gmail.com>
2023-10-26 04:08:41 -0700

    binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * binary_to_uint - convert binary number
+ * @b: input binary number
+ *
+ *
+ *Return: decimal
+ *
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int decimal;
+	int  i;
+
+	decimal = 0;
+	if (!b)
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		decimal <<= 1;
+		if (b[i] == '1')
+			decimal = decimal + 1;
+		i++;
+	}
+	return (decimal);
+}
