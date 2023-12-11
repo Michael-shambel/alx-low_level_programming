76976076e4f3a44ad1f248b3e8e2eebe9dc08ec9
Michael Shambel <Michaelshambel10@gmail.com>
2023-12-11 02:15:49 -0800

    calc

---
+++ 0x18-dynamic_libraries/calculator.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+/**
+ *add - add two number
+ *@a: first number
+ *@b: second number
+ *
+ * Return: sum of two number
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ *sub - subsraction of two number
+ *@a: first number
+ *@b: second number
+ *
+ * Return : substract of two number
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ *mul - multiply two number
+ *@a: first number
+ *@b: second number
+ *
+ * Return: multiplication of two number
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ *div - division of two numbers
+ *@a: first number
+ *@b: second number
+ *
+ *
+ * Return: division of two number
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ *mod - modulur of two number
+ *@a: first number
+ *@b: second number
+ *
+ * Return: the reminder og two number
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
