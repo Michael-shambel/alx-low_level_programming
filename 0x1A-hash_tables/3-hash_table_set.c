4e9bfb0d8eeb6ef0241046608907779167589efd
Michael Shambel <Michaelshambel10@gmail.com>
2023-12-22 14:05:56 -0800

    error handled

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,9 +1,11 @@
 #include "hash_tables.h"
 /**
+ * hash_table_set - add element to the table
+ *@ht: hash table
+ *@key: key value
+ *@value: the value
  *
- *
- *
- *
+ * Return: 1 if completed
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
@@ -12,10 +14,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
-
-	index = key_index((const unsigned char *)key, ht->size);
+	index = key_index((unsigned char *)key, ht->size);
 	now = ht->array[index];
-
 	while (now != NULL)
 	{
 		if (strcmp(now->key, key) == 0)
@@ -28,25 +28,21 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		now = now->next;
 	}
-
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 	{
-		free(new_node);
 		return (0);
 	}
-
+	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-	if (new_node->value == NULL)
+	if (new_node->value == NULL || new_node->key == NULL)
 	{
 		free(new_node->key);
+		free(new_node->value);
 		free(new_node);
 		return (0);
 	}
-
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-
 	return (1);
 }
-
