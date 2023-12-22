aae90f62080f347321ecbf5a5de4c75536b06b0a
Michael Shambel <Michaelshambel10@gmail.com>
2023-12-22 13:41:38 -0800

    error

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,18 +1,20 @@
 #include "hash_tables.h"
 /**
+ *hash_djb2 - hash algorism
+ *@str: string input
  *
- *
- *
+ * Return: hash
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	hash = 5381;
+
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
