31a7ca5708d4943badacb09184c7f428ee7aa8b0
michael-shambel <Michaelshambel10@gmail.com>
2024-04-13 01:17:39 -0700

    task 0

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -10,19 +10,28 @@
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t low = 0;
-	size_t  high = size - 1;
+	size_t low;
+	size_t  high;
 	size_t pos;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	low = 0;
+	high = size - 1;
 
 	 pos = low + (((double)(high - low) / (array[high] - array[low])) * (value -
 			array[low]));
 
-	if (array == NULL)
-		return (-1);
 	while (low <= high && value >= array[low] && value <= array[high])
 	{
 		pos = low + (((double)(high - low) / (array[high] - array[low])) * (value -
 				array[low]));
+		if (pos >= size)
+		{
+			printf("Value checked array[%lu] is out of range\n", pos);
+			return (-1);
+		}
 		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 		if (array[pos] == value)
 			return (pos);
@@ -35,6 +44,6 @@ int interpolation_search(int *array, size_t size, int value)
 			high = pos - 1;
 		}
 	}
-	printf("Value checked array[%lu] is out of range\n", pos);
+	printf("Value not found\n");
 	return (-1);
 }
